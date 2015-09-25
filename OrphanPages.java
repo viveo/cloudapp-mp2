18a9818ee880d4325e76367fbf460a0a85e0b1b9
viveo <viveo.hao@gmail.com>
2015-09-25 23:22:35 +0000

    Modify OrphanPages.java to find orphan pages in Wikipedia

--- OrphanPages.java
+++ OrphanPages.java
@@ -15,6 +15,10 @@ import org.apache.hadoop.util.ToolRunner;
 import java.io.IOException;
 import java.util.StringTokenizer;
 
+
+// my code ???
+import org.apache.hadoop.fs.FileSystem;
+
 // >>> Don't Change
 public class OrphanPages extends Configured implements Tool {
 
@@ -26,20 +30,56 @@ public class OrphanPages extends Configured implements Tool {
 
     @Override
     public int run(String[] args) throws Exception {
-        //TODO
+	// my code
+        Configuration conf = this.getConf();
+        FileSystem fs = FileSystem.get(conf);
+        Path tmpPath = new Path("/mp2/tmp");
+        fs.delete(tmpPath, true);
+
+        Job jobA = Job.getInstance(conf, "Orphan Page");
+        jobA.setOutputKeyClass(IntWritable.class);
+        jobA.setOutputValueClass(NullWritable.class);
+
+	jobA.setMapOutputKeyClass(IntWritable.class);
+        jobA.setMapOutputValueClass(IntWritable.class);
+
+        jobA.setMapperClass(LinkCountMap.class);
+        jobA.setReducerClass(OrphanPageReduce.class);
+
+        FileInputFormat.setInputPaths(jobA, new Path(args[0]));
+	FileOutputFormat.setOutputPath(jobA, new Path(args[1]));
+
+        jobA.setJarByClass(OrphanPages.class);
+        return jobA.waitForCompletion(true) ? 0 : 1;
     }
 
     public static class LinkCountMap extends Mapper<Object, Text, IntWritable, IntWritable> {
         @Override
         public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-            //TODO
+		// my code
+                String curLine = value.toString();
+		String[] parts = curLine.split(":");
+                context.write(new IntWritable(Integer.parseInt(parts[0])), new IntWritable(0));
+		StringTokenizer tokenizer = new StringTokenizer(parts[1]);
+
+                while(tokenizer.hasMoreTokens()) {
+                        String nextToken = tokenizer.nextToken();
+                        context.write(new IntWritable(Integer.parseInt(nextToken.trim().toLowerCase())), new IntWritable(1));
+                }
         }
     }
 
     public static class OrphanPageReduce extends Reducer<IntWritable, IntWritable, IntWritable, NullWritable> {
         @Override
         public void reduce(IntWritable key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
-            //TODO
+		// my code
+		int sum = 0;
+		for(IntWritable val : values) {
+			sum += val.get();
+		}
+		if(sum == 0) {
+			context.write(key, NullWritable.get());
+		}
         }
     }
 }
